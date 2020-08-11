e05ec7377ae23224d7a01c35283f59d1ec5ff7f1
Deepak Kumar Chouhan <dk397787@gmail.com>
2020-08-11 17:02:48 +0530

    Updates

---
+++ precache-manifest.bf73d06abcf5b90d662bbb24aecf962b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a72ccae384797ba5fced68864e064e2",
+    "url": "/reactrestaurantcrud/index.html"
+  },
+  {
+    "revision": "cf9329265dd28ff54a01",
+    "url": "/reactrestaurantcrud/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "54f31cde584931cee7f2",
+    "url": "/reactrestaurantcrud/static/css/main.779505f5.chunk.css"
+  },
+  {
+    "revision": "cf9329265dd28ff54a01",
+    "url": "/reactrestaurantcrud/static/js/2.6d16e09b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactrestaurantcrud/static/js/2.6d16e09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f31cde584931cee7f2",
+    "url": "/reactrestaurantcrud/static/js/main.f2862489.chunk.js"
+  },
+  {
+    "revision": "842fb95c2bbc5dd0703e",
+    "url": "/reactrestaurantcrud/static/js/runtime-main.7cad4730.js"
+  },
+  {
+    "revision": "fe654734afe03d1e33db05828379d9f5",
+    "url": "/reactrestaurantcrud/static/media/foods.fe654734.jpg"
+  }
+]);
